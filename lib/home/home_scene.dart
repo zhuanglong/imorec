88f5e2ef754d0ae28e01198843cd7fd0482dcf4b
zhuanglong <2766428466@qq.com>
2020-05-05 18:09:41 +0800

    no message

--- lib/home/home_scene.dart
+++ lib/home/home_scene.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:imorec/app/app_color.dart';
 
 class HomeScene extends StatefulWidget {
   @override
@@ -8,6 +11,20 @@ class HomeScene extends StatefulWidget {
 class _HomeSceneState extends State<HomeScene> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        title: Text('首页'),
+        backgroundColor: AppColor.white,
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
